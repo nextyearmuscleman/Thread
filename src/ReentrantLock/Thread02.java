0bc771242b917330c5f82aed411263546398be41
jixuelei <36285784+nextyearmuscleman@users.noreply.github.com>
2018-10-25 22:27:19 +0800

    Update Thread02.java

--- src/ReentrantLock/Thread02.java
+++ src/ReentrantLock/Thread02.java
@@ -13,6 +13,18 @@ import java.util.concurrent.locks.ReentrantLock;
  * 搭配try{...}catch{...}
  *
  * 这个例子中，需要2个线程互斥，那么2个线程使用同意把lock锁就可以了
+ 
+ 4中加锁方法的总结：
+ 假如线程A和线程B使用同一个锁LOCK，此时线程A首先获取到锁LOCK.lock()，并且始终持有不释放。如果此时B要去获取锁，有四种方式：
+
+    1、LOCK.lock(): 此方式会始终处于等待中，即使调用B.interrupt()也不能中断，除非线程A调用LOCK.unlock()释放锁。
+    2、LOCK.lockInterruptibly(): 此方式会等待，但当调用B.interrupt()会被中断等待，并抛出InterruptedException异常，
+    否则会与lock()一样始终处于等待中，直到线程A释放锁。
+    3、LOCK.tryLock(): 该处不会等待，获取不到锁并直接返回false，去执行下面的逻辑。
+    4、LOCK.tryLock(10, TimeUnit.SECONDS)：该处会在10秒时间内处于等待中，但当调用B.interrupt()会被中断等待，
+    并抛出InterruptedException。10秒时间内如果线程A释放锁，会获取到锁并返回true，否则10秒过后会获取不到锁并返回
+    false，去执行下面的逻辑。
+
  */
 public class Thread02 {
     Lock lock = new ReentrantLock();
